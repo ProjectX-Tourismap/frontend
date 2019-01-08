eda6ce0fdb453efa9e69795bad2f39d46ee5f3f9
syuchan1005 <syuchan.dev@gmail.com>
2019-01-08 20:37:22 +0900

    :heavy_exclamation_mark: Change server address

--- src/util/AxiosMock.js
+++ src/util/AxiosMock.js
@@ -10,7 +10,7 @@ if (process.env.NODE_ENV !== 'production') {
   console.info('%cNow use AxiosMock!', 'color: red;');
   const mock = new AxiosMockAdapter(axios);
 
-  mock.onGet(/https?:\/\/backend.syuchan.work\/api/).reply((config) => {
+  mock.onGet(/https?:\/\/backend.syuchan.site\/api/).reply((config) => {
     const { query } = config.params;
     const queryName = query.substring(1, query.indexOf('('));
     if (queryName === 'nearEntitiesInPoint') return [200, nearEntitiesInPointMockData];
